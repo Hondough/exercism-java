4d1de97735a4e68cda5d6ab6918b8927d1033b40
John Hannemann <johnkhannemann@gmail.com>
2017-07-02 15:14:30 -0500

    Imported Optional for Robot.java Brought in new exercism challenge - sieve.

--- robot-name/src/main/java/Robot.java
+++ robot-name/src/main/java/Robot.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
