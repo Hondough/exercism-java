dd946a9f24aa3174bee0a99ef42e262c10a8125c
John Hannemann <johnkhannemann@gmail.com>
2017-07-02 13:16:38 -0500

    Solution

--- perfect-numbers/src/main/java/Classification.java
+++ perfect-numbers/src/main/java/Classification.java
@@ -1,5 +1,17 @@
+import java.util.stream.IntStream;
+
 enum Classification {
 
-    DEFICIENT, PERFECT, ABUNDANT
+    DEFICIENT, PERFECT, ABUNDANT;
+
+    public static Classification createClassification(int n) {
+
+        int aliquotSum = IntStream.range(1, n)
+            .parallel()
+            .filter(i -> n % i == 0)
+            .sum();
+
+        return aliquotSum == n ? PERFECT : aliquotSum < n ? DEFICIENT : ABUNDANT;
+    }
 
 }
--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,20 @@
 class NaturalNumber {
 
+    private int value;
+    private Classification classification;
 
+    public int getValue() {
+        return value;
+    }
+    public Classification getClassification() {
+        return classification;
+    }
 
+    public NaturalNumber(int value) {
+        if (value < 1) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+        this.value = value;
+        this.classification = Classification.createClassification(value);
+    }
 }
